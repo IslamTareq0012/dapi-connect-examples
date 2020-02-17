abce0b4c7c32bd19b49c9af5489b7d9224688140
Kamil Kamili <kamilkamili@kaaddevelopers.com>
2020-02-18 01:51:15 +0400

    upgraded flutter example project to work with the new system

--- webviews/flutter_sample_webview/lib/main.dart
+++ webviews/flutter_sample_webview/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -27,66 +28,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   //create the url with query parameters
-  String generateConnectInitializationUrl(){
+  String generateConnectInitializationUrl() {
     var baseUrl = "connect.dapi.co";
     var config = {
-      "appKey": "c1606405091dc6f0f0b9ab34dec5f9c404042bcef7ceafa4c2527074947cb5d0",
+      "appKey":
+          "31b0d52278655aedeed11600012fd803458e87db5d2d9bbc6e96fb5136de6256",
       "environment": "sandbox",
-      "redirectUri": "https://google.com",
-      "isMobile" : "true",
-      "isWebview": "true"
+      "isMobile": "true",
+      "isWebview": "true",
+      "countries": json.encode(["AE"])
     };
 
     return Uri.https(baseUrl, '', config).toString();
-    
   }
-
 
   // Parse the URL to determine if it's a special Connect redirect or a request
   // Handle connect redirects and open traditional pages directly in the user's
   // preferred browser.
-  NavigationDecision myNavigationDelegate(NavigationRequest navigationRequest){
+  NavigationDecision myNavigationDelegate(NavigationRequest navigationRequest) {
     var connectActionType = "dapiconnect";
     var uri = Uri.parse(navigationRequest.url);
-    if(uri.scheme == connectActionType){
+    if (uri.scheme == connectActionType) {
       var action = uri.host;
       var data = parsedUriData(uri);
 
-      if(action == 'connected'){
+      if (action == 'connected') {
         //TODO: Close webview after receiving the access_code and continue with app flow
+
+        var accessCode = data['access_code'].replaceAll(' ', '+');
+        var connectionID = data['connection_id'].replaceAll(' ', '+');
+        var userSecret = data['user_secret'].replaceAll(' ', '+');
 
         //Successfully linked the user bank. Now exchange this access_code with access_token
         //to fetch user related bank information.
-        print("Access Code: ${data['access_code']}");
-
-      } else if (action == 'exit'){
-
+        print("Access Code: ${accessCode}");
+        print("User secret: ${userSecret}");
+        print("Connection ID: ${connectionID}");
+      } else if (action == 'exit') {
         //TODO: Close webview
       } else if (action == 'event') {
-
         // The event action is fired as the user moves through the Dapi Connect
         print("Event Name: ${data['event_name']}");
-
-      } else if (action == 'error'){
-
+      } else if (action == 'error') {
         // The error action is fired wheneever an error happened
         // either wrong configuration params or wrong username/password
         print("Error Type: ${data['error_type']}");
         print("Error Message: ${data['error_message']}");
       }
       return NavigationDecision.prevent;
-    } else if (uri.scheme == 'https' || uri.scheme == 'http'){
+    } else if (uri.scheme == 'https' || uri.scheme == 'http') {
       return NavigationDecision.navigate;
     }
-
   }
 
   //parse query parameters into a Dictionary
-  HashMap<String, String> parsedUriData(Uri uri){
+  HashMap<String, String> parsedUriData(Uri uri) {
     var data = HashMap<String, String>();
-    for(var key in uri.queryParameters.keys){
+    for (var key in uri.queryParameters.keys) {
       data[key] = uri.queryParameters[key];
     }
     return data;
@@ -94,17 +93,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     // Generate the Connect initialization URL based off of the configuration options.
     String connectUrl = generateConnectInitializationUrl();
-    
+
     print("Connection URl: ${connectUrl}");
 
     return Scaffold(
       body: WebView(
-          key: UniqueKey(),
-          initialUrl: connectUrl,
-          javascriptMode: JavascriptMode.unrestricted,
+        key: UniqueKey(),
+        initialUrl: connectUrl,
+        javascriptMode: JavascriptMode.unrestricted,
         navigationDelegate: myNavigationDelegate,
       ),
     );
